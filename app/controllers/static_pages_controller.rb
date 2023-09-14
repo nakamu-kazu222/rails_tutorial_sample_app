8617a7ad1a98e358db405b85ca10fff18d55fff0
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-09-14 16:58:09 +0900

    homeアクションにマイクロポストのインスタンス変数を追加した

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,13 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
+  end
+
+  def about
   end
 
   def contact
